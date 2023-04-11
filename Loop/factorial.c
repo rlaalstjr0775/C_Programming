8540d4627ae89173534036318f257b38b2be307a
rlaalstjr0775 <rlaalstjr0775@gmail.com>
2023-04-12 02:13:32 +0900

    Create factorial.c
    Write a program that reads a nonnegative integer and computes and prints its factorial.

---
+++ Loop/factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i, result = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = num; i > 0; i--)
+    {
+        result = result * i;
+    }
+
+    printf("%d! = %d\n", num, result);
+}
